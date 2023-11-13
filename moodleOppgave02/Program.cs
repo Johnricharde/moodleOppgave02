710a62114a80761393495939c467116b8625bf95
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-13 13:42:17 +0100

    Add project files.

---
+++ moodleOppgave02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace moodleOppgave02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer      = 1;
+            long longInteger = 1;
+
+            float floatInteger     = 1.5f;
+            double doubleInteger   = 1.5d;
+            decimal decimalInteger = 1.5m;
+
+            char character    = 'a';
+            string characters = "abcd";
+
+            bool isTrue = true;
+
+
+            Console.WriteLine($"integer: {integer}\nlong integer: {longInteger}\n");
+            Console.WriteLine($"float: {floatInteger}\ndouble: {doubleInteger}\ndecimal: {decimalInteger}\n");
+            Console.WriteLine($"character: {character}\ncharacters: {characters}\n");
+            Console.WriteLine($"isTrue: {isTrue}");
+
+            Console.ReadKey();
+        }
+    }
+}
